e5df848584b4faa19e4ac0aa1c38ecc682d68fda
olaoluwa vincent <olaoluwavincent@gmail.com>
2022-10-14 17:12:56 +0100

    Feat: Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+	content: ['./*.{html,js}'],
+	theme: {
+		extend: {},
+		colors: {
+			red: 'hsl(0, 78%, 62%)',
+			cyan: 'hsl(180, 62%, 55%)',
+			orange: 'hsl(34, 97%, 64%)',
+			blue: 'hsl(212, 86%, 64%)',
+			'dark-blue': 'hsl(234, 12%, 34%)',
+			'gray-blue': 'hsl(234, 12%, 34%)',
+		},
+		fontFamily: {
+			sans: ['Poppins', 'sans-serif'],
+		},
+	},
+	plugins: [],
+};
